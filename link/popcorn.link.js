cc4738d468dd58ba4c8b8435a91387d72c4574e6
Ian Hunter <beanland2@gmail.com>
2013-07-09 22:06:13 -0600

    Stubbed out link plugin.

---
+++ link/popcorn.link.js
@@ -0,0 +1,18 @@
+/**
+ * Stub for link plugin until we work out how we want it to work  
+ */
+(function (Popcorn) {
+   Popcorn.plugin( "link", (function(){
+       
+       return {
+            _setup: function( options ){
+            },
+            start: function( event, options ){
+            },
+            end: function( event, options ){
+            },
+            _tearDown: function( options ){
+            }
+       }
+   })());
+})(Popcorn);
