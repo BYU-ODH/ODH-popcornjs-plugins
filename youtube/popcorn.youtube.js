0a49a4fa139df9b7d632c94b5fca94bd09bd39fd
Ian Hunter <beanland2@gmail.com>
2013-07-11 12:04:20 -0600

    YouTube video thumb displayer plugin added.

---
+++ youtube/popcorn.youtube.js
@@ -0,0 +1,59 @@
+/**
+ * Example:
+ * pop.freebase({
+ *      start: 10,
+ *      end: 15,
+ *      target: "elementID",
+ *      key: "YOURFREEBASEAPIKEY",
+ *      item: "/m/somethingorother"
+ * });
+ */
+(function ( Popcorn ) {
+   Popcorn.plugin( "youtube", ( function() {
+       var api = "https://www.googleapis.com/youtube/v3/search";
+
+       return {
+           _setup: function( options ) {
+               var url = api + '?maxResults=5&part=snippet&q=' + options.item,
+                   el = document.createElement('div'),
+                   txt = document.createTextNode('Loading...');
+
+               if(options.key) { url += "&key=" + options.key; }
+
+               el.className = 'popcorn-plugin-youtube';
+               options._el = el;
+               el.appendChild(txt);
+               el.style.display = 'none';
+               Popcorn.dom.find(options.target).appendChild(el);
+
+               Popcorn.getJSONP( url, function populateData( data ) {
+                   if(options._el) { // does the element still exist? If not, don't do anything
+                       el.removeChild(txt);
+
+                       var list = document.createElement('ul');
+
+                       /**@TODO: not sure what to populate this with yet **/
+                       for(var i = 0; i<data.items.length; i++) {
+                           var item = document.createElement('li');
+                           var img = document.createElement('img');
+                           img.src = data.items[i].snippet.thumbnails.default.url;
+                           item.appendChild(img);
+                           list.appendChild(item);
+                       };
+                       el.appendChild(list);
+                   };
+               });
+           },
+           start: function( event, options ) {
+               options._el.style.display = 'block';              
+           },
+           end: function( event, options ) {
+               options._el.style.display = 'none';              
+           },        
+           _teardown: function( options ) {
+               options._el.parentNode.removeChild(options._el);
+               delete options._el;
+           }
+       };    
+   })());
+})( Popcorn );
