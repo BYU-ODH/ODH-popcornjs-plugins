73c35f397dafe98039335ce78f83d72fd6ad325e
Ian Hunter <beanland2@gmail.com>
2013-10-03 13:37:31 -0600

    Blanking method added.

---
+++ blank/popcorn.blank.js
@@ -0,0 +1,44 @@
+(function (Popcorn) {
+   Popcorn.plugin( "blank", (function(){
+       var css    = '.popcorn-plugin-blank { visibility: hidden; } video.popcorn-plugin-blank::-webkit-media-controls { visibility: visible; border: solid white 1px; box-shadow: 0px 0px 2px 2px black; background-color: black; } video.popcorn-plugin-blank::-webkit-media-controls:before { color: white; content: "Content customized for BYU"; font-weight: bold; }',
+           style  = document.createElement('style');
+
+       style.innerHTML = css;
+       document.head.appendChild(style);
+
+       return {
+            // Define a manifest for the butter authoring tool to use
+            manifest: {
+                // Plugin meta data
+                // will be used in the butter ui
+                about:{
+                    name: "Blank"
+                },
+                // Object representation of the plugin options
+                // a form will be constructed against this object
+                options:{
+                    start : {elem:'input', type:'text', label:'Blank From'},
+                    end : {elem:'input', type:'text', label:'To'},
+                    target : {hidden: true}
+                }
+            },
+            _setup: function( options ){
+            },
+
+            start: function( event, options ){
+                this.video.className += ' popcorn-plugin-blank';
+            },
+
+            end: function( event, options ){
+                this.video.className = this.video.className.replace( /\bpopcorn-plugin-blank\b/g, '');
+            },
+
+            toString: function(){
+                return "Blank";
+            },
+                    
+            _teardown: function( options ){
+            }
+       }
+   })());
+})(Popcorn);
