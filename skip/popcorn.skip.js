4e8f7747eac05a7e97dc20525fc479e4f9f736e1
Ian Hunter <beanland2@gmail.com>
2013-12-13 14:05:36 -0700

    Skip plugin now obeys Popcorn's disabled list.

--- skip/popcorn.skip.js
+++ skip/popcorn.skip.js
@@ -36,8 +36,24 @@
                 }
             },
             _setup: function( options ){
-                var pop = this;
+                var pop             = this,
+                    hasDisabledList = ( (pop.data && pop.data.disabled) !== undefined );
+
                 options._skip = function() {
+
+                    /**
+                     * Normal operation would be based in the start/end methods.
+                     * When a Popcorn instance has a disabled plugin, its start
+                     * methods aren't called. Since we're using a timeupdate we need
+                     * to check to make sure skipping hasn't been explicitly disabled.
+                     * 
+                     * I'm not sure if all Popcorn versions contain pop.data.disabled,
+                     * which is why I've created the boolean hasDisabledList
+                     */
+                    if( hasDisabledList && pop.data.disabled.skip ) {
+                        return;
+                    }
+
                     var ct = pop.currentTime();
                     if (ct >= options.end || ct < options.start) {
                         return;
